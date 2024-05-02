025b8105ae463fdb2082779d6c84b1027328256c
nikita <tele2.rumerkulov@yandex.ru>
2024-05-02 10:24:42 +0500

    Sprint_7

--- endpoints/create_courier_objects.py
+++ endpoints/create_courier_objects.py
@@ -12,21 +12,6 @@ class CreateCourier:
         self.response = None
         self.login_pass = None
 
-    # Метод создания курьера
-    def create_courier(self):
-        self.login_pass = self.register_new_courier_and_return_login_password()
-        login, password, first_name = self.login_pass
-        self.response = requests.post(f'{URL}api/v1/courier', data={
-            "login": login,
-            "password": password,
-            "firstName": first_name
-        })
-        self.response_status = self.response.status_code
-
-    # Метод проверяет статус код после создания курьера
-    def check_create_courier_is_201(self):
-        assert self.response.status_code == 201
-
     # Генерируем login, password, first_name
     def register_new_courier_and_return_login_password(self):
         # метод генерирует строку, состоящую только из букв нижнего регистра, в качестве параметра передаём длину строки
@@ -42,3 +27,19 @@ class CreateCourier:
 
         # возвращаем список с учетными данными
         return [login, password, first_name]
+
+    # Метод создания курьера
+    def create_courier(self):
+        self.login_pass = self.register_new_courier_and_return_login_password()
+        login, password, first_name = self.login_pass
+        self.response = requests.post(f'{URL}api/v1/courier', data={
+            "login": login,
+            "password": password,
+            "firstName": first_name
+        })
+        self.response_status = self.response.status_code
+
+    # Метод проверяет статус код после создания курьера
+    def check_create_courier_is_201(self):
+        assert self.response.status_code == 201
+
