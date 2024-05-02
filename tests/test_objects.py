22d011be596435e357b665554eee2ca44480b5b9
nikita <tele2.rumerkulov@yandex.ru>
2024-05-02 10:26:01 +0500

    Sprint_7

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -2,6 +2,6 @@ from endpoints.create_courier_objects import CreateCourier
 
 
 def test_create_courier():
-    new_test = CreateCourier()
-    new_test.create_courier()
-    new_test.check_create_courier_is_201()
+    new_create_courier = CreateCourier()
+    new_create_courier.create_courier()
+    new_create_courier.check_create_courier_is_201()
