89020b6def93b472511a5c42afeec789251eec11
nikita <tele2.rumerkulov@yandex.ru>
2024-05-03 10:56:21 +0500

    Sprint_7

--- tests/data.py
+++ tests/data.py
@@ -64,16 +64,20 @@ ORDER = {
 }
 
 ORDER_LIST = {'orders':
-            [{'id': 236364, 'courierId': 298008, 'firstName': 'Петр', 'lastName': 'Петров', 'address': 'ул. Ленина 11',
-              'metroStation': '2', 'phone': '89504564545', 'rentTime': 1, 'deliveryDate': '2024-02-23T19:00:00.000Z',
-              'track': 61731, 'color': ['BLACK'], 'comment': '', 'createdAt': '2024-01-29T15:35:30.471Z',
-              'updatedAt': '2024-05-03T05:08:10.469Z', 'status': 1},
-             {'id': 275644, 'courierId': 298008, 'firstName': 'Петр', 'lastName': 'Петров', 'address': 'ул. Ленина 11',
-              'metroStation': '2', 'phone': '89504564545', 'rentTime': 1, 'deliveryDate': '2024-02-23T19:00:00.000Z',
-              'track': 61731, 'color': ['BLACK'], 'comment': '', 'createdAt': '2024-05-03T05:08:10.488Z',
-              'updatedAt': '2024-05-03T05:08:10.488Z', 'status': 1}],
-        'pageInfo': {'page': 0, 'total': 2, 'limit': 30}, 'availableStations':
-            [{'name': 'Черкизовская', 'number': '2', 'color': '#D92B2C'}]}
+                  [{'id': 236364, 'courierId': 298008, 'firstName': 'Петр', 'lastName': 'Петров',
+                    'address': 'ул. Ленина 11',
+                    'metroStation': '2', 'phone': '89504564545', 'rentTime': 1,
+                    'deliveryDate': '2024-02-23T19:00:00.000Z',
+                    'track': 61731, 'color': ['BLACK'], 'comment': '', 'createdAt': '2024-01-29T15:35:30.471Z',
+                    'updatedAt': '2024-05-03T05:08:10.469Z', 'status': 1},
+                   {'id': 275644, 'courierId': 298008, 'firstName': 'Петр', 'lastName': 'Петров',
+                    'address': 'ул. Ленина 11',
+                    'metroStation': '2', 'phone': '89504564545', 'rentTime': 1,
+                    'deliveryDate': '2024-02-23T19:00:00.000Z',
+                    'track': 61731, 'color': ['BLACK'], 'comment': '', 'createdAt': '2024-05-03T05:08:10.488Z',
+                    'updatedAt': '2024-05-03T05:08:10.488Z', 'status': 1}],
+              'pageInfo': {'page': 0, 'total': 2, 'limit': 30}, 'availableStations':
+                  [{'name': 'Черкизовская', 'number': '2', 'color': '#D92B2C'}]}
 
 MESSAGE_CHECK_CREATE_DUPLICATE = 'Этот логин уже используется. Попробуйте другой.'
 MESSAGE_CHECK_CREATE_COURIER_EMPTY = 'Недостаточно данных для создания учетной записи'
