4ed7d0cafe680e357c5ef39156abbee86e977c8d
nikita <tele2.rumerkulov@yandex.ru>
2024-05-03 10:36:30 +0500

    Sprint_7

--- endpoints/base_endpoints.py
+++ endpoints/base_endpoints.py
@@ -1,7 +1,3 @@
-import random
-import string
-
-
 class Endpoints:
     response = None
 
@@ -13,20 +9,3 @@ class Endpoints:
 
     def check_response_is_400(self):
         assert self.response.status_code == 400
-
-    # Генерируем login, password, first_name
-    def register_new_courier_and_return_login_password(self):
-        # метод генерирует строку, состоящую только из букв нижнего регистра, в качестве параметра передаём длину строки
-        def generate_random_string(length):
-            letters = string.ascii_lowercase
-            random_string = ''.join(random.choice(letters) for i in range(length))
-            return random_string
-
-        # генерируем логин, пароль и имя курьера
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
-
-        # возвращаем словарь с учетными данными
-        payload = {"login": login, "password": password, "firstName": first_name}
-        return payload
--- endpoints/create_courier_objects.py
+++ endpoints/create_courier_objects.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 import requests
 from endpoints.base_endpoints import Endpoints
 
@@ -5,6 +8,23 @@ from tests.data import URL, CREATE_DUPLICATE_COURIER, MESSAGE_CHECK_CREATE_DUPLI
 
 
 class CreateCourier(Endpoints):
+
+    # Генерируем login, password, first_name
+    def register_new_courier_and_return_login_password(self):
+        # метод генерирует строку, состоящую только из букв нижнего регистра, в качестве параметра передаём длину строки
+        def generate_random_string(length):
+            letters = string.ascii_lowercase
+            random_string = ''.join(random.choice(letters) for i in range(length))
+            return random_string
+
+        # генерируем логин, пароль и имя курьера
+        login = generate_random_string(10)
+        password = generate_random_string(10)
+        first_name = generate_random_string(10)
+
+        # возвращаем словарь с учетными данными
+        payload = {"login": login, "password": password, "firstName": first_name}
+        return payload
 
     # Метод создания курьера с уникальными данными
     def create_courier(self):
